f3984a162ac2388e5a2c385a4f34148d2dcc6daf
Eric Chauvin <ericsourcecode@gmail.com>
2018-03-06 08:52:21 -0700

    Create SpaceObject.cs

---
+++ SpaceObject.cs
@@ -0,0 +1,37 @@
+// Copyright Eric Chauvin 2018.
+// My blog is at:
+// ericsourcecode.blogspot.com
+
+
+// This is any object in space.  A planet, space
+// ship, the Sun, or whatever.
+
+
+using System;
+// using System.Collections.Generic;
+using System.Text;
+// using System.Threading.Tasks;
+// using System.Windows.Media;
+// using System.Windows.Media.Media3D;
+// using System.Windows.Media.Imaging;
+
+
+namespace ClimateModel
+{
+  abstract class SpaceObject
+  {
+  internal double X;
+  internal double Y;
+  internal double Z;
+  // internal double Mass;
+  // internal double VelocityX;
+  // internal double VelocityY;
+  // internal double VelocityZ;
+
+
+
+  abstract internal GeometryModel3D MakeGeometryModel();
+
+
+  }
+}
