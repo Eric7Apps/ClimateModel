eced81764927c13129d3d2d8454fc1ddc8fb2241
Eric Chauvin <ericsourcecode@gmail.com>
2018-03-15 12:23:01 -0600

    Update SpaceObject.cs

--- SpaceObject.cs
+++ SpaceObject.cs
@@ -11,9 +11,9 @@ using System;
 // using System.Collections.Generic;
 using System.Text;
 // using System.Threading.Tasks;
-// using System.Windows.Media;
-// using System.Windows.Media.Media3D;
-// using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using System.Windows.Media.Media3D;
+using System.Windows.Media.Imaging;
 
 
 namespace ClimateModel
