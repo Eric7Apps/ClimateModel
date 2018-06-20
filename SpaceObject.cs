43e9a26d2e6f422b65c41e92e954afa6144f0f01
Eric Chauvin <ericsourcecode@gmail.com>
2018-06-20 13:49:00 -0600

    Update SpaceObject.cs

--- SpaceObject.cs
+++ SpaceObject.cs
@@ -1,6 +1,7 @@
 // Copyright Eric Chauvin 2018.
 // My blog is at:
-// ericsourcecode.blogspot.com
+// https://scientificmodels.blogspot.com/
+
 
 
 // This is any object in space.  A planet, space
@@ -8,9 +9,7 @@
 
 
 using System;
-// using System.Collections.Generic;
 using System.Text;
-// using System.Threading.Tasks;
 using System.Windows.Media;
 using System.Windows.Media.Media3D;
 using System.Windows.Media.Imaging;
@@ -20,12 +19,43 @@ namespace ClimateModel
 {
   abstract class SpaceObject
   {
+  protected MainForm MForm;
+  internal string ObjectName = "";
   internal Vector3.Vector Position;
   internal Vector3.Vector Velocity;
   internal Vector3.Vector Acceleration;
   internal double Mass;
-  // The time as seen from this local system.
-  // internal double LocalClock;
+  internal JPLHorizonsData JPLData;
+
+
+
+  private SpaceObject()
+    {
+
+    }
+
+
+
+  internal SpaceObject( MainForm UseForm,
+                        string UseName,
+                        string JPLFileName )
+    {
+    MForm = UseForm;
+    ObjectName = UseName;
+    JPLData = new JPLHorizonsData( MForm );
+    JPLData.ReadFromTextFile( JPLFileName );
+    }
+
+
+
+  internal void ShowStatus( string ToShow )
+    {
+    if( MForm == null )
+      return;
+
+    MForm.ShowStatus( ToShow );
+    }
+
 
 
   abstract internal void MakeNewGeometryModel();
@@ -42,6 +72,41 @@ namespace ClimateModel
     // It moves by this much in TimeDelta time.
     Vector3.MultiplyWithScalar( ref MoveBy, TimeDelta );
     Vector3.Add( ref Position, ref MoveBy );
+    }
+
+
+
+
+  internal void SetToNearestJPLPosition( ulong NearestDate )
+    {
+    JPLHorizonsData.JPLRec Rec = JPLData.GetNearestRecByDateTime( NearestDate );
+    // ShowStatus( "Rec.CalDate is:" );
+    if( Rec.CalDate == 0 )
+      {
+      ShowStatus( "No JPL data for: " + ObjectName );
+      return;
+      }
+
+    // ECTime ShowTime = new ECTime( Rec.CalDate );
+    // ShowStatus( ShowTime.ToCrudeString());
+
+    Position.X = Rec.PositionX;
+    Position.Y = Rec.PositionY;
+    Position.Z = Rec.PositionZ;
+
+    Velocity.X = Rec.VelocityX;
+    Velocity.Y = Rec.VelocityY;
+    Velocity.Z = Rec.VelocityZ;
+
+    ShowStatus( " " );
+    ShowStatus( ObjectName );
+    ShowStatus( "Position.X: " + Position.X.ToString( "N1" ));
+    ShowStatus( "Position.Y: " + Position.Y.ToString( "N1" ));
+    ShowStatus( "Position.Z: " + Position.Z.ToString( "N1" ));
+
+    ShowStatus( "Velocity.X: " + Velocity.X.ToString( "N1" ));
+    ShowStatus( "Velocity.Y: " + Velocity.Y.ToString( "N1" ));
+    ShowStatus( "Velocity.Z: " + Velocity.Z.ToString( "N1" ));
     }
 
 
